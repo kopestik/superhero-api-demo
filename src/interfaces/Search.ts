83bc90f46efa84e74f4e8c0a28b6253432ed5cc2
Nicolas <kopestik.dev@gmail.com>
2023-02-07 06:56:17 +0800

    add types for superhero details endpoint

--- src/interfaces/Search.ts
+++ src/interfaces/Search.ts
@@ -1,3 +1,12 @@
+import {
+  Appearance,
+  Biography,
+  Connections,
+  Image,
+  Powerstats,
+  Work,
+} from "./Superhero";
+
 export interface Search {
   response: string;
   "results-for": string;
@@ -13,46 +22,4 @@ export interface Result {
   work: Work;
   connections: Connections;
   image: Image;
-}
-
-export interface Powerstats {
-  intelligence: string;
-  strength: string;
-  speed: string;
-  durability: string;
-  power: string;
-  combat: string;
-}
-
-export interface Biography {
-  "full-name": string;
-  "alter-egos": string;
-  aliases: string[];
-  "place-of-birth": string;
-  "first-appearance": string;
-  publisher: string;
-  alignment: string;
-}
-
-export interface Appearance {
-  gender: string;
-  race: string;
-  height: string[];
-  weight: string[];
-  "eye-color": string;
-  "hair-color": string;
-}
-
-export interface Work {
-  occupation: string;
-  base: string;
-}
-
-export interface Connections {
-  "group-affiliation": string;
-  relatives: string;
-}
-
-export interface Image {
-  url: string;
 }
